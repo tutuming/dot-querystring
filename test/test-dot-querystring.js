fc83b8d93014902642cdc3d951ab8eb8dd7d75ea
tutuming <tutuming@gmail.com>
2012-03-17 15:13:26 +0900

    add test

--- test/test-dot-querystring.js
+++ test/test-dot-querystring.js
@@ -1,44 +1,52 @@
-var dotQuerystring = require('../lib/dot-querystring');
+try{
+  var dotQs = require('../lib/dot-querystring');
+} catch (x) {
+  //ignore
+}
 
-describe('dotQuerystring.parse', function(){
+describe('dotQs.parse', function(){
   it('shold ignore left-hand only', function(){
-    dotQuerystring.parse('aaa').should.eql({});
+    dotQs.parse('aaa').should.eql({});
   });
 
   it('shold parse object', function(){
-    dotQuerystring.parse('aaa=bbb').should.eql({'aaa' : 'bbb'});
+    dotQs.parse('aaa=bbb').should.eql({'aaa' : 'bbb'});
+  });
+
+  it('shold parse object(when the key begins with number)', function(){
+    dotQs.parse('111aaa=bbb').should.eql({'111aaa' : 'bbb'});
   });
 
   it('shold parse array', function(){
-    dotQuerystring.parse('0=bbb&1=ccc').should.eql(['bbb', 'ccc']);
+    dotQs.parse('0=bbb&1=ccc').should.eql(['bbb', 'ccc']);
   });
 
   it('shold use type of appeared first', function(){
-    dotQuerystring.parse('0=1&a=2').should.eql(['1']);
-    dotQuerystring.parse('a=2&0=1').should.eql({a : '2'});
+    dotQs.parse('0=1&a=2').should.eql(['1']);
+    dotQs.parse('a=2&0=1').should.eql({a : '2'});
   });
 
   it('should skip empty array indexes', function(){
-    dotQuerystring.parse('3=3').should.eql([,,,'3']);
+    dotQs.parse('3=3').should.eql([,,,'3']);
   });
 
   it('should parse nested', function(){
-    dotQuerystring.parse('a.b.c=1&a.b.d=2').should.eql({a : {b : {c : '1', d : '2'}}});
-    dotQuerystring.parse('a.0.a=1&a.0.b=2&a.1.a=3&a.1.b=4').should.eql({a : [{a : '1', b : '2'}, {'a' : '3', 'b' : '4'}]});
+    dotQs.parse('a.b.c=1&a.b.d=2').should.eql({a : {b : {c : '1', d : '2'}}});
+    dotQs.parse('a.0.a=1&a.0.b=2&a.1.a=3&a.1.b=4').should.eql({a : [{a : '1', b : '2'}, {'a' : '3', 'b' : '4'}]});
   });
 });
 
-describe('dotQuerystring.flatten', function(){
+describe('dotQs.flatten', function(){
   it('should flatten object', function(){
-    dotQuerystring.flatten({a : {b : 1, c : 2}}).should.eql({'a.b' : 1, 'a.c' : 2});
+    dotQs.flatten({a : {b : 1, c : 2}}).should.eql({'a.b' : 1, 'a.c' : 2});
   });
 
   it('should flatten array', function(){
-    dotQuerystring.flatten([1,2,3]).should.eql({'0' : 1, '1' : 2, '2' : 3});
+    dotQs.flatten([1,2,3]).should.eql({'0' : 1, '1' : 2, '2' : 3});
   });
 
   it('should flatten nested', function(){
-    dotQuerystring.flatten({
+    dotQs.flatten({
       a : 12345,
       b : {
         c : 'hoge',
@@ -54,19 +62,19 @@ describe('dotQuerystring.flatten', function(){
 });
 
 
-describe('dotQuerystring.stringify', function(){
+describe('dotQs.stringify', function(){
   it('should stringify basic objects', function(){
-    dotQuerystring.stringify({a : 1}).should.eql('a=1');
-    dotQuerystring.stringify({a : 'あ'}).should.eql('a=%E3%81%82');
+    dotQs.stringify({a : 1}).should.eql('a=1');
+    dotQs.stringify({a : 'あ'}).should.eql('a=%E3%81%82');
   });
 
   it('should stringify basic array', function(){
-    dotQuerystring.stringify([1,2,3,4,5]).should.eql('0=1&1=2&2=3&3=4&4=5');
-    dotQuerystring.stringify(['あ','い']).should.eql('0=%E3%81%82&1=%E3%81%84');
+    dotQs.stringify([1,2,3,4,5]).should.eql('0=1&1=2&2=3&3=4&4=5');
+    dotQs.stringify(['あ','い']).should.eql('0=%E3%81%82&1=%E3%81%84');
   });
 
   it('should stringify nested object', function(){
-    dotQuerystring.stringify({
+    dotQs.stringify({
       a : 12345,
       b : {
         c : 'hoge',
